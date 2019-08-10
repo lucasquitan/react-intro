9bb663f9126b43c9e5a17f20ee1601d00467506e
Lucas Quintanilha <l.quitan@gmail.com>
2019-08-10 13:53:51 -0300

    Importando imagens no React

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './App.css';
 
+import profile from './assets/profile.png';
+
 function App() {
-  return <h1>Hello Rocketseat</h1>;
+  return <img width="350" src={profile} />;
 }
 
 export default App;
