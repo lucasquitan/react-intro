9285af7966d90d1e5eaa4b21e1b137c1b8ed10bf
Lucas Quintanilha <l.quitan@gmail.com>
2019-08-10 11:47:17 -0300

    Configuração do webpack e babel

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Fazer o navegador entender as funcionalidades do js
+    '@babel/preset-react', // Fazer o navegador entender as funcionalidades do react
+  ],
+};
